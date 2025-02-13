f358493645d9ecbb3a99798aef67774d72211d88
EuwinC <114059468+EuwinC@users.noreply.github.com>
2025-02-13 15:38:45 +0800

    Update Group_Anagrams.py

--- NeetCode/Array/Group_Anagrams/Group_Anagrams.py
+++ NeetCode/Array/Group_Anagrams/Group_Anagrams.py
@@ -3,28 +3,24 @@ class Solution1:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashmap = collections.defaultdict(list)
 
-        for s in strs:
-            list_s = list(s)
-            list_s.sort()
+        for s in strs: #O(N)
+            list_s = list(s) 
+            list_s.sort() #O(Nlog(N))
             list_s = "".join(list_s)
             hashmap[list_s].append(s)
         res =[]
-        for anagram in hashmap:
+        for anagram in hashmap: #O(N)
             res.append(hashmap.get(anagram))
         return res
 
-class Solution2(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
+class Solution2:
+    def groupAnagrams(self, strs) -> List[List[str]]:
 
         res =defaultdict(list)
 
-        for s in strs:
+        for s in strs: #O(N)
             freq = [0] * 26
-            for c in s:
+            for c in s: #O(N)
                 freq[ord(c)-ord('a')] +=1
             res[tuple(freq)].append(s)
         return res.values()
